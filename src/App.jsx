c74e0ae3f5ce2b629105415a8d094815cf3066d1
ChrisGuter <christian.gutermann1337@gmail.com>
2025-04-04 18:52:20 +0200

    added the about section with placeholders
    + has to be modified to match my Infos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import About from "./components/About";
 import Header from "./components/Header";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <>
       <div className="pt-[4.75rem] lg:pt-5.25 overflow-hidden">
         <Header />
+        <About />
       </div>
       <ButtonGradient />
     </>
