2820493de04cb5f5ec595bb97118f438f2c7c58b
ChrisGuter <christian.gutermann1337@gmail.com>
2025-04-29 00:46:09 +0200

    Refactoring Benefits -> Recent Work
    + renaming and adapting imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import About from "./components/About";
 import Expertise from "./components/Expertise";
 import Header from "./components/Header";
-
+import RecentWork from "./components/RecentWork";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-5.25 overflow-hidden">
         <Header />
         <About />
+        <RecentWork />
         <Expertise />
       </div>
       <ButtonGradient />
