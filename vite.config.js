be8b2ac27246d08fedcb3ea948e58da2b94c1aff
ChrisGuter <christian.gutermann1337@gmail.com>
2025-05-07 23:23:21 +0200

    added the crypto-browserify plugin to properly build the project and remove warnings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    nodePolyfills(), // Add this plugin to polyfill Node.js modules
+  ],
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify", // Use the browser-compatible crypto module
+    },
+  },
+});
