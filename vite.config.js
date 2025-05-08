274c74aae2760d0a617b29f945bcc3a91965dbaa
ChrisGuter <christian.gutermann1337@gmail.com>
2025-05-08 02:03:20 +0200

    changed vite config to fix build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(), // Add this plugin to polyfill Node.js modules
-  ],
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify", // Use the browser-compatible crypto module
-    },
-  },
+  plugins: [react()],
 });
