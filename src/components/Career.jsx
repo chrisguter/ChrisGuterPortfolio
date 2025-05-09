76ac359ecc8e53cd163aad654c3e48a1e3eb3430
ChrisGuter <christian.gutermann1337@gmail.com>
2025-05-09 18:47:34 +0200

    added i18n to the career section

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -5,70 +5,75 @@ import Tagline from "./Tagline";
 import { career } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
+import { useTranslation } from "react-i18next";
 
-const Career = () => (
-  <Section className="overflow-hidden" id="career" crosses>
-    <div className="container md:pb-10">
-      <Heading tag="Career" title="My professional journey" />
+const Career = () => {
+  const { t } = useTranslation();
 
-      <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
-        {career.map((item) => {
-          const status = item.status === "done" ? "Done" : "In progress";
+  return (
+    <Section className="overflow-hidden" id="career" crosses>
+      <div className="container md:pb-10">
+        <Heading tag={t("career.tag")} title={t("career.heading")} />
 
-          return (
-            <div
-              className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
-                item.colorful ? "bg-conic-gradient" : "bg-n-6"
-              }`}
-              key={item.id}
-            >
-              <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
-                <div className="absolute top-0 left-0 max-w-full">
-                  <img
-                    className="w-full"
-                    src={grid}
-                    width={550}
-                    height={550}
-                    alt="Grid"
-                  />
-                </div>
-                <div className="relative z-1">
-                  <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
-                    <Tagline>{item.date}</Tagline>
+        <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+          {career.map((item) => {
+            const status = item.status === "done" ? "Done" : "In progress";
 
-                    <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
-                      <img
-                        className="mr-2.5"
-                        src={item.status === "done" ? check2 : loading1}
-                        width={16}
-                        height={16}
-                        alt={status}
-                      />
-                      <div className="tagline">{status}</div>
-                    </div>
-                  </div>
-
-                  <div className="mb-10 -my-10 -mx-15">
+            return (
+              <div
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                  item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                }`}
+                key={item.id}
+              >
+                <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                  <div className="absolute top-0 left-0 max-w-full">
                     <img
                       className="w-full"
-                      src={item.imageUrl}
-                      width={628}
-                      height={426}
-                      alt={item.title}
+                      src={grid}
+                      width={550}
+                      height={550}
+                      alt="Grid"
                     />
                   </div>
-                  <h4 className="h4 mb-4">{item.title}</h4>
-                  <p className="body-2 text-n-4">{item.text}</p>
+                  <div className="relative z-1">
+                    <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
+                      <Tagline>{t(item.date)}</Tagline>
+
+                      <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
+                        <img
+                          className="mr-2.5"
+                          src={item.status === "done" ? check2 : loading1}
+                          width={16}
+                          height={16}
+                          alt={status}
+                        />
+                        <div className="tagline">{status}</div>
+                      </div>
+                    </div>
+
+                    <div className="mb-10 -my-10 -mx-15">
+                      <img
+                        className="w-full"
+                        src={item.imageUrl}
+                        width={628}
+                        height={426}
+                        alt={t(item.title)}
+                      />
+                    </div>
+                    <h4 className="h4 mb-4">{t(item.title)}</h4>
+                    <p className="body-2 text-n-4">{t(item.text)}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
 
-        <Gradient />
+          <Gradient />
+        </div>
       </div>
-    </div>
-  </Section>
-);
+    </Section>
+  );
+};
 
 export default Career;
